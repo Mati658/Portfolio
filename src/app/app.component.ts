4deea03206b0a36300fbe1344a7e50b95e3e9de1
Mati658 <matiasaltamira@outlook.com>
2025-03-23 14:00:20 -0300

    Portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,181 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AfterViewInit, Component, ElementRef, inject, OnInit, signal, ViewChild } from '@angular/core';
+import 'swiper/css';
+import { CommonModule } from '@angular/common';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { SwiperContainer } from 'swiper/element';
+import { SwiperOptions } from 'swiper/types';
+import emailjs from 'emailjs-com';
+import { environment } from '../environments/environment';
+import Swal from 'sweetalert2'
+import { FormsModule } from '@angular/forms';
+import { TranslatePipe } from '@ngx-translate/core';
+
+import { TranslateLoader, TranslateService } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export function HttpLoaderFactory(http: HttpClient): TranslateLoader {
+  return {
+    getTranslation: (lang: string): Observable<any> =>
+      http.get(`./i18n/${lang}.json`)
+  };
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule, 
+    FormsModule,
+    TranslatePipe
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'Portfolio';
+export class AppComponent implements OnInit, AfterViewInit{
+
+  swiperElement = signal<SwiperContainer | null>(null);
+  @ViewChild('formElement') formElement!: ElementRef;
+  translate = inject(TranslateService);
+  btnAnterior : any;
+
+  btnAnteriorLng : any;
+
+  name: string = '';
+  email: string = '';
+  title: string = '';
+  message: string = '';
+
+  constructor(){
+    this.translate.setDefaultLang('es');
+  }
+
+  cambiarIdioma(lang: string) {
+    this.translate.use(lang);
+    const button = document.getElementById(lang);
+    if (button) {
+      if (button == this.btnAnteriorLng) {
+        return;
+      }
+
+      button.style.color = "#14AE5C";
+
+      if (this.btnAnteriorLng) {
+        this.btnAnteriorLng.style.color = "aliceblue";
+      }
+
+      this.btnAnteriorLng = button;
+    }
+  }
+
+  ngOnInit(): void {
+    const swiperElemConstructor = document.querySelector('swiper-container');
+    const swiperOptions:SwiperOptions = {
+      slidesPerView : 1,
+      pagination: true,
+      autoplay:true,
+      loop:true,
+      breakpoints:{
+        300:{slidesPerView: 1},
+        640:{slidesPerView: 2},
+        980:{slidesPerView: 3},
+      },
+      spaceBetween:30,
+    };
+    Object.assign(swiperElemConstructor!, swiperOptions);
+    this.swiperElement.set(swiperElemConstructor as SwiperContainer);
+    this.swiperElement()?.initialize();
+
+  }
+
+  ngAfterViewInit(): void {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          const sectionId = entry.target.id; 
+          this.botonSelect(`btn-${sectionId}`);
+        }
+      });
+    }, { threshold: 0.5 }); 
+
+    document.querySelectorAll('div').forEach(section => observer.observe(section));
+
+    this.btnAnteriorLng = document.getElementById('es');
+    if (this.btnAnteriorLng) {
+      this.btnAnteriorLng.style.color = "#14AE5C";
+    }
+  }
+
+  enviarMail() {
+    if (this.name && this.email && this.title && this.message) {
+  
+      const form = this.formElement.nativeElement;
+  
+      emailjs.sendForm(environment.SERVICE_ID, environment.TEMPLATE_ID, form, environment.USER_ID)
+        .then((response) => {
+          console.log('Correo enviado!', response);
+          Swal.fire({
+            title: "Enviado!",
+            icon: "success",
+            background: "#1E1E1E"
+          });
+        })
+        .catch((error) => {
+          console.error('Error al enviar el correo:', error);
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Algo salió mal!",
+            background: "#1E1E1E"
+          });
+        });
+    } else {
+      const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.onmouseenter = Swal.stopTimer;
+          toast.onmouseleave = Swal.resumeTimer;
+        }
+      });
+      Toast.fire({
+        icon: "error",
+        title: "Complete todos los campos!",
+        background: "#1E1E1E"
+      });
+      console.log("Formulario inválido, algunos campos están vacíos");
+    }
+  }
+
+  GoSection(sectionId:any){
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth'});
+      if (sectionId=="home") {
+          window.scrollTo({ top: 0, behavior: 'smooth' });
+      }
+    }
+  }
+
+  botonSelect(idButton:string = ""){
+    const button = document.getElementById(idButton);
+    if (button) {
+      if (button == this.btnAnterior) {
+        return;
+      }
+
+      button.style.color = "#14AE5C";
+
+      if (this.btnAnterior) {
+        this.btnAnterior.style.color = "aliceblue";
+      }
+
+      this.btnAnterior = button;
+    }
+  }
+
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,24 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { HttpLoaderFactory } from './app.component';
+
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]        }
+      })
+    )
+  ]
 };
